bc7f1cb897bd2b0dcc43a54dc10f5d0ddfebd5e6
wang111chi <2271685756@qq.com>
2016-01-20 13:12:46 +0800

    more_func
    added more function

--- controllers/api.py
+++ controllers/api.py
@@ -42,85 +42,77 @@ def MD5Sign(md5Str):
     m.update(md5Str)
     return m.hexdigest().upper()
 
-#@auth.requires_login()
-@request.restful()
-def TradeAdd():
-    response.view ='generic.'+request.extension  #return  json
-    def POST(tablename,**vars):
-        msg=[]
-        jsonObj={}
-        keys=vars.keys()
-        for k in keys:
-            k.decode('unicode_escape')
-        if ('apiKey' in keys)and('product_info' in keys)and\
-        ('timestamp' in keys)and('sig' in keys) and\
-        ('out_tid' in keys)and ('shop_id' in keys)and\
-        ('consignee' in keys)and('address' in keys)and\
-        ('postcode' in keys)and('mobilPhone' in keys)and\
-        ('order_date' in keys)and\
-        ('product_title' in vars['product_info'][0].keys())and \
-        ('standard' in vars['product_info'][0].keys())and\
-        ('out__tid' in vars['product_info'][0].keys()):
-            ts=int(time.time())
-            this_apiKey='A6BEA59B'
-            this_apiSecret='1F6F088755B094DDAD3C7AEFEA73A1A1'
-            essential_field=vars.keys()
-            essential_field.sort()
-            SigStr=""
-            for field in essential_field:
-                if field =="sig" or field =="apiSecret":
+def EdbAPI(**arg):
+    def _EdbAPI(func):
+        def __EdbAPI(data):
+            #url="http://vip79.edb04.net/edb2/rest/openapi/index.aspx"
+            url="http://vip79.edb04.net/rest/index.aspx"
+            SysData={
+                "appkey":"5a7b7896",
+                "dbhost":"edb_a77527",
+                "format":"json",
+                "v":2.0,
+                "slencry":0,
+                "Ip":"117.79.148.228",
+                }
+            ExData={
+                "appscret":"1f5b75edd28d480e968feecbc38f2c73",
+                "token":"7041e7424cb4410f8370f10a2d3a285a",
+                }
+            param=func(data)
+            param.update(SysData)
+            param["method"]=arg["method"]
+            param["timestamp"]=GetTimeStamp()
+            paraList=[]
+            for k,v in param.items():
+                if k=="appkey":
                     continue
-                elif field =="product_info":
-                    SigStr=SigStr+unicode(field)+u"="+u"["
-                    for d in vars["product_info"]:
-                        k=d.keys()
-                        k.sort()
-                        SigStr=SigStr+u"{"
-                        for j in k:
-                            SigStr=SigStr+unicode(j)+u"="+unicode(d[j])+u"&"
-                        SigStr=SigStr[0:len(SigStr)-1]
-                        SigStr=SigStr+u"}"
-                    SigStr=SigStr+u"]"+u"&"
-                else:
-                    SigStr=SigStr+unicode(field)+u"="+unicode(vars[field])+u"&"
-            this_sig=MD5Sign((SigStr[0:len(SigStr)-1]+this_apiSecret).encode('utf8'))
-            if (vars['sig']==this_sig) and (ts-int(vars['timestamp'])<180) and \
-            ((vars['apiKey']).decode('unicode_escape')==(this_apiKey)):
-                msg.append({'is_success':'true','response_Msg':u'成功导入系统'})
-                content={}
-                for field in essential_field:
-                    if field =="sig" or field =="apiKey" or field=="timestamp":
-                        continue
-                    elif field=="product_info":
-                        for f in vars["product_info"][0].keys():
-                            content[f]=vars["product_info"][0][f]
-                    else:
-                        content[field]=vars[field]
-                data={"order_id":'out_tid',"status": "0"}
-                updateTOS(data)
+                if not str(v):
+                    continue
+                paraList.append(k+str(v))
+            for k,v in ExData.items():
+                paraList.append(k+str(v))
+            paraList.sort(cmp)
+            md5Str="".join(paraList)
+            md5Str=SysData["appkey"]+"appkey"+SysData["appkey"]+md5Str
+            param["sign"]=MD5Sign(md5Str)
+            req=requests.post(url,param)
+            if not req.ok:
+                print "requests FAIL"
+                log_file("%s %s"%(arg,data))
+                filePath=log_file(req.content)
+                print "log_file in",filePath
+                return
+            return json.loads(req.content)
+        return __EdbAPI
+    return _EdbAPI
 
-                order_phone=db().select(db.trade.mobilPhone).as_list()
-                if not {'mobilPhone':content['mobilPhone']} in order_phone:
-                    data["status"]=1
-                    db.trade.insert(**content)
-                else:
-                    data["status"]=2
-                    data["messge"]=u"订单重复"
-                updateTOS(data)
-            else:
-                msg.append({'is_success':'false','response_Msg':'wrong identifier params'})
-        else:
-            msg.append({'is_success':'false','response_Msg':'absence of essential field',})
-        jsonObj['item']=msg
-        return jsonObj
-    return locals()
-
+@EdbAPI(method="edbTradeAdd")
+def AddTrade(data):
+    orderTag=('out_tid','shop_id','storage_id','buyer_id','buyer_msg','buyer_email','buyer_alipay','seller_remark','consignee','address','postcode','telephone','mobilPhone','privince','city','area','actual_freight_get','actual_RP','ship_method','express','is_invoiceOpened','invoice_type','invoice_money','invoice_title','invoice_msg','order_type','process_status','pay_status','deliver_status','is_COD','serverCost_COD','order_totalMoney','product_totalMoney','pay_method','pay_commission','pay_score','return_score','favorable_money','alipay_transaction_no','out_payNo','out_express_method','out_order_status','order_date','pay_date','finish_date','plat_type','distributor_no','WuLiu','WuLiu_no','in_memo','other_remark','actual_freight_pay','ship_date_plan','deliver_date_plan','is_scorePay','is_needInvoice')
+    productTag=('barCode','product_title','standard','out_price','favorite_money','orderGoods_Num','gift_Num','cost_Price','tid','product_stockout','is_Book','is_presell','is_Gift','avg_price','product_freight','shop_id','out_tid','out_productId','out_barCode','product_intro')
+    orderXml=XML_ET.fromstring('<info><orderInfo></orderInfo></info>')
+    productXml=XML_ET.fromstring('<product_info><product_item></product_item></product_info>')
+    orderInfo=orderXml.find("orderInfo")
+    itemInfo=productXml.find("product_item")
+    for k,v in data.items():
+        if k in orderTag:
+            tmp=XML_ET.SubElement(orderInfo,k)
+            tmp.text=v.decode('utf8')
+        if k in productTag:
+            tmp=XML_ET.SubElement(itemInfo,k)
+            tmp.text=v.decode('utf8')
+    orderInfo.append(productXml)
+    finalStr=XML_ET.tostring(orderXml,"utf8")
+    result={}
+    result["xmlValues"]=finalStr[37:]
+    return result
 
 def TrialAPI(**arg):
     def _TrialAPI(func):
         def __TrialAPI(data):
-            SysData={"apiKey":"qhT49hGFv5rks",}
-            ExData={"apiSecret":"wh76BtGfks7fVbkiu",}
+            SysData={"apiKey":"qhT49hGFv5rks"}
+            ExData={"apiSecret":"wh76BtGfks7fVbkiu"}
             param=func(data)
             param.update(SysData)
             param.update(data)
@@ -149,6 +141,92 @@ def updateOTI(data):
 @TrialAPI(method="updateTrialOrderStatus")
 def updateTOS(data):
     return {}
+
+#@auth.requires_login()
+@request.restful()
+def AddTrade2Edb():
+    response.view ='generic.'+request.extension  #return  json
+    def POST(tablename,**vars):
+        msg=[]
+        jsonObj={}
+        keys=vars.keys()
+        for k in keys:
+            k.decode('unicode_escape')
+        if ('apiKey' in keys)and('product_info' in keys)and\
+        ('timestamp' in keys)and('sig' in keys) and\
+        ('out_tid' in keys)and ('shop_id' in keys)and\
+        ('consignee' in keys)and('address' in keys)and\
+        ('postcode' in keys)and('mobilPhone' in keys)and\
+        ('order_date' in keys)and('storage_id' in keys)and\
+        ('barCode' in vars['product_info'][0].keys())and\
+        ('product_title' in vars['product_info'][0].keys())and\
+        ('standard' in vars['product_info'][0].keys())and\
+        ('out__tid' in vars['product_info'][0].keys()):
+            ts=int(time.time())
+            this_apiKey='A6BEA59B'
+            this_apiSecret='1F6F088755B094DDAD3C7AEFEA73A1A1'
+            essential_field=vars.keys()
+            essential_field.sort()
+            SigStr=""
+            for field in essential_field:
+                if field =="sig" or field =="apiSecret":
+                    continue
+                elif field =="product_info":
+                    SigStr=SigStr+unicode(field)+u"="+u"["
+                    for d in vars["product_info"]:
+                        k=d.keys()
+                        k.sort()
+                        SigStr=SigStr+u"{"
+                        for j in k:
+                            SigStr=SigStr+unicode(j)+u"="+unicode(d[j])+u"&"
+                        SigStr=SigStr[0:len(SigStr)-1]
+                        SigStr=SigStr+u"}"
+                    SigStr=SigStr+u"]"+u"&"
+                else:
+                    SigStr=SigStr+unicode(field)+u"="+unicode(vars[field])+u"&"
+            this_sig=MD5Sign((SigStr[0:len(SigStr)-1]+this_apiSecret).encode('utf8'))
+            all_barcode=db().select(db.AllBarcode.bar_code).as_list()
+            if (vars['sig']==this_sig) and (ts-int(vars['timestamp'])<180) and \
+            ((vars['apiKey']).decode('unicode_escape')==(this_apiKey))and\
+            (vars['product_info'][0]['barCode'] in all_barcode):
+                msg.append({'is_success':'true','response_Msg':'import to TaoTongGroup successfully'})
+                content={}
+                for field in essential_field:
+                    if field =="sig" or field =="apiKey" or field=="timestamp":
+                        continue
+                    elif field=="product_info":
+                        for f in vars["product_info"][0].keys():
+                            content[f]=vars["product_info"][0][f]
+                    else:
+                        content[field]=vars[field]
+                data={"order_id":'out_tid',"status": "0"}
+                updateTOS(data)
+                order_phone=db().select(db.trade.mobilPhone).as_list()
+                if not {'mobilPhone':content['mobilPhone']} in order_phone:
+                    data["status"]=1
+                    db.trade.insert(**content)
+                else:
+                    data["status"]=2
+                    data["messge"]="duplicate trade order"
+                updateTOS(data)
+                edb_data={}
+                edb_data={
+                    "out_tid":vars["out_tid"],
+                    "shop_id":vars['shop_id'],
+                    "storage_id":vars['storage_id'],
+                    "order_date":vars['order_date'],
+                    "barCode":vars['product_info'][0]['barCode'],
+                    "product_title":vars['product_info'][0]['product_title'],
+                    "standard":vars['product_info'][0]['standard'],
+                    "orderGoods_Num":"1"
+                }
+            else:
+                msg.append({'is_success':'false','response_Msg':'wrong identifier params sig or barCode'})
+        else:
+            msg.append({'is_success':'false','response_Msg':'absence of essential field'})
+        jsonObj['item']=msg
+        return jsonObj
+    return locals()
 
 #@auth.requires_login()
 @request.restful()
